e8dd158bf2c6776466a59f732d17f2c28a21720b
Your Name <you@example.com>
2026-07-03 19:24:32 -0400

    v1

---
+++ src/main.py
@@ -0,0 +1,127 @@
+from signalrcore.hub_connection_builder import HubConnectionBuilder
+import logging
+import requests
+import json
+import time
+from dotenv import load_dotenv
+import os
+import sqlite3
+
+
+
+class App:
+    def __init__(self):
+        load_dotenv()  # charge .env
+
+        self._hub_connection = None
+        self.TICKS = 10
+
+        # Variables d'environnement
+        self.HOST = os.getenv("HOST")
+        self.TOKEN = os.getenv("TOKEN")
+        self.T_MAX = float(os.getenv("T_MAX"))
+        self.T_MIN = float(os.getenv("T_MIN"))
+        self.DATABASE_URL = os.getenv("DATABASE_URL")
+
+        # Connexion BD initialisée une seule fois
+        self.db = self.init_db()
+
+    def init_db(self):
+        """Initialize database connection and create table if needed."""
+        conn = sqlite3.connect(self.DATABASE_URL)
+        cursor = conn.cursor()
+
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS measurements (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                timestamp TEXT NOT NULL,
+                temperature REAL NOT NULL,
+                action TEXT NOT NULL
+            );
+        """)
+
+        conn.commit()
+        return conn
+
+    def __del__(self):
+        if self._hub_connection != None:
+            self._hub_connection.stop()
+
+    def start(self):
+        """Start Oxygen CS."""
+        self.setup_sensor_hub()
+        self._hub_connection.start()
+        print("Press CTRL+C to exit.")
+        while True:
+            time.sleep(2)
+
+    def setup_sensor_hub(self):
+        """Configure hub connection and subscribe to sensor data events."""
+        self._hub_connection = (
+            HubConnectionBuilder()
+            .with_url(f"{self.HOST}/SensorHub?token={self.TOKEN}")
+            .configure_logging(logging.INFO)
+            .with_automatic_reconnect(
+                {
+                    "type": "raw",
+                    "keep_alive_interval": 10,
+                    "reconnect_interval": 5,
+                    "max_attempts": 999,
+                }
+            )
+            .build()
+        )
+        self._hub_connection.on("ReceiveSensorData", self.on_sensor_data_received)
+        self._hub_connection.on_open(lambda: print("||| Connection opened."))
+        self._hub_connection.on_close(lambda: print("||| Connection closed."))
+        self._hub_connection.on_error(
+            lambda data: print(f"||| An exception was thrown closed: {data.error}")
+        )
+
+    def on_sensor_data_received(self, data):
+        """Callback method to handle sensor data on reception."""
+        try:
+            print(data[0]["date"] + " --> " + data[0]["data"], flush=True)
+            timestamp = data[0]["date"]
+            temperature = float(data[0]["data"])
+
+            action = self.take_action(temperature)
+            self.save_event_to_database(timestamp, temperature, action)
+        except Exception as err:
+            print(err)
+
+    def take_action(self, temperature):
+        """Take action to HVAC depending on current temperature."""
+        if float(temperature) >= float(self.T_MAX):
+            self.send_action_to_hvac("TurnOnAc")
+            return "TurnOnAc"
+        elif float(temperature) <= float(self.T_MIN):
+            self.send_action_to_hvac("TurnOnHeater")
+            return "TurnOnHeater"
+        return "None"
+
+
+    def send_action_to_hvac(self, action):
+        """Send action query to the HVAC service."""
+        r = requests.get(f"{self.HOST}/api/hvac/{self.TOKEN}/{action}/{self.TICKS}")
+        details = json.loads(r.text)
+        print(details, flush=True)
+
+    def save_event_to_database(self, timestamp, temperature, action):
+        """Save sensor data into database."""
+        try:
+            cursor = self.db.cursor()
+            cursor.execute(
+                "INSERT INTO measurements (timestamp, temperature, action) VALUES (?, ?, ?)",
+                (timestamp, temperature, action)
+            )
+            self.db.commit()
+            print("Saved to DB:", timestamp, temperature, action)
+        except Exception as e:
+            print("DB error:", e)
+
+
+
+if __name__ == "__main__":
+    app = App()
+    app.start()
